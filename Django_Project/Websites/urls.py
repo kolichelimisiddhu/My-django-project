f3fc0153226d96f4e8086523b0dff5feeb796007
kolichelimisiddhu <kolichelimisiddhu@gmail.com>
2025-12-06 10:45:15 +0530

    WS-2 Added a simple form after login

---
+++ Django_Project/Websites/urls.py
@@ -0,0 +1,30 @@
+"""
+URL configuration for Websites project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from .views import render_page, register_page,home_page_render,home_page_view
+
+urlpatterns = [
+    path('', render_page, name='login'),
+    path('homepage/',home_page_render,name="home"),
+    path('viewpage/', home_page_view, name="view"),
+    path('register/', register_page, name='register'),  
+    
+
+]
+
